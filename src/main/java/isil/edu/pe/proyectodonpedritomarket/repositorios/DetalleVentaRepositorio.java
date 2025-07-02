f2a555b5aa980886306fe1382d83e2b54882c724
Jercas1304 <jere716324@gmail.com>
2025-07-02 14:40:43 -0500

    Adicion Repositorio
    Venta y DetalleVenta

---
+++ src/main/java/isil/edu/pe/proyectodonpedritomarket/repositorios/DetalleVentaRepositorio.java
@@ -0,0 +1,9 @@
+package isil.edu.pe.proyectodonpedritomarket.repositorios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import isil.edu.pe.proyectodonpedritomarket.modelo.DetalleVenta;
+
+public interface DetalleVentaRepositorio extends JpaRepository<DetalleVenta, Integer>{
+
+}
